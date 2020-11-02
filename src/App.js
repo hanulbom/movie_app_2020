80e19cd39771090afccaf0024b0ef9a3b576c593
WooMin <hanulbom@gmail.com>
2020-11-02 23:13:09 +0900

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     count: 0,
   };
   add = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
+    this.setState((current) => ({ count: current.count + 1 }));
   };
   minus = () => {
     this.setState((current) => ({ count: current.count - 1 }));
@@ -16,7 +16,7 @@ class App extends React.Component {
       <div>
         <h1>The number is: {this.state.count}</h1>
         <button onClick={this.add}>Add</button>
-        <button onClink={this.minus}>Minus</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
     );
   }
