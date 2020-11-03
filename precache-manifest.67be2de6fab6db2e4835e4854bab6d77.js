dd1a2e9f726e1ea505fe7e20e23e2a36ef6e344b
WooMin <hanulbom@gmail.com>
2020-11-03 18:45:12 +0900

    Updates

---
+++ precache-manifest.67be2de6fab6db2e4835e4854bab6d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8ae75116315862219d8ba71f4f1154",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1f835f5a6d131eac2a7c",
+    "url": "/movie_app_2020/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "56b49c2a4b8b295d9f5b",
+    "url": "/movie_app_2020/static/js/2.db1e8dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.db1e8dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f835f5a6d131eac2a7c",
+    "url": "/movie_app_2020/static/js/main.9b593366.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
